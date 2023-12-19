0cfd6416ccc94bc2ce2d8cdcf02463d5f9deabee
nunkesser <robin.nunkesser@hshl.de>
2023-12-19 11:39:39 +0100

    Add list items

--- sales-organizer-app/src/app/pages/list/list.page.ts
+++ sales-organizer-app/src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import {AlertController} from '@ionic/angular';
 import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonButton, IonNav, IonItem, IonLabel, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
@@ -16,11 +17,24 @@ export class ListPage implements OnInit {
   {number:2,title: 'Trip to Berlin'},
   {number:3,title: 'Board Meeting'}];
 
-  addTask() {
-    console.log('Add task')    
+  async addTask() {
+    const alert = await this.alertCtrl.create({
+      header: 'Add Task',
+      inputs: [{
+        name: 'title',
+        placeholder: 'Task title',
+        type: 'text'
+      }],
+      buttons: [
+        {text:'Cancel',
+      role:'cancel'},
+        {text:'Add',
+      handler: (data) => {this.tasks.push(data)}}]
+    });
+    await alert.present();
   }
 
-  constructor() {}
+  constructor(public alertCtrl: AlertController) {}
 
   ngOnInit() {    
   }
